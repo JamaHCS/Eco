a395092ec6ede09144c87d11e45c748045fae0e7
Jama <jama117@hotmail.com>
2019-04-05 12:30:19 -0600

    pruebas: push.js/notificacion.js

---
+++ notificacion.js
@@ -0,0 +1,24 @@
+/*
+Push.create("Holi", {
+  body: "Holi, raza'?",
+  icon: 'assets/ico.png',
+  timeout: 10000,
+  onClick: function() {
+    window.focus();
+    this.close();
+  }
+});
+ */
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function() {
+  Push.create("Holi", {
+    body: "Holi, raza'?",
+    icon: 'assets/icoo.png',
+    timeout: 10000,
+    onClick: function() {
+      window.focus();
+      this.close();
+    }
+  });
+})
